8bb14ce7b237ad9123fb47bdf5d457ea4783468d
amal m <mail.vortextv@gmail.com>
2022-07-09 00:34:16 +0530

    just added to path router-simple barebones with some decorations

--- main.go
+++ main.go
@@ -15,8 +15,22 @@ func contactFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact me at pillai_amal@hotmail.com</h1>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		handlerFunc(w, r)
+		break
+	case "/contact":
+		contactFunc(w, r)
+		break
+	default:
+		fmt.Println("not in list")
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.HandleFunc("/contact", contactFunc)
+	http.HandleFunc("/", pathHandler)
+	fmt.Println("Go to http://localhost:3000/ in your browser")
+	fmt.Println("Server started and is listening to the port 3000")
 	http.ListenAndServe(":3000", nil)
 }
