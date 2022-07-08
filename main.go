ae4b9206ed25183b1fada36923a48d9dd00f8894
amal m <mail.vortextv@gmail.com>
2022-07-08 23:50:36 +0530

    barebones - just added one more page

--- main.go
+++ main.go
@@ -6,10 +6,17 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content", "text/html")
 	fmt.Fprint(w, "<h1>Hello to my site!</h1>")
+}
+
+func contactFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content", "text/html")
+	fmt.Fprint(w, "<h1>Contact me at pillai_amal@hotmail.com</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/contact", contactFunc)
 	http.ListenAndServe(":3000", nil)
 }
