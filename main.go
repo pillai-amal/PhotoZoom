5e4288cfb31aa47b24951e9a2dada2c28efb36b4
amal m <mail.vortextv@gmail.com>
2022-07-10 19:12:52 +0530

    added some basic erroe handling using log and http.StatusInternalError

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
+	"text/template"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content", "text/html")
-	fmt.Fprint(w, "<h1>Hello to my site!</h1>")
+	tmplPath := filepath.Join("templates", "home.gohtml")
+	t, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		log.Printf("%v error was occured on parsing", err)
+		http.Error(w, "the error caused the program to quit", http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Printf("%v error was occured on template execution", err)
+		http.Error(w, "the error caused the program to quit", http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactFunc(w http.ResponseWriter, r *http.Request) {
